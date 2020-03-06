bf53b29ba649013e6ec78c80bc034696e44b38ab
Dr Franck P. Vidal <fpvidal@linux-oitr.ad.bangor.ac.uk>
2020-03-06 14:33:07 +0000

    A bit of debugging, moved where the database is created. Added comments where the authorid has to be checked.

--- BibViz.py
+++ BibViz.py
@@ -17,107 +17,116 @@ if len(sys.argv) == 1:
 # There is at least one bibtex file to read
 else:
 
-        # Create an empty list
-        bib_database_set = [];
+    # Create an empty list
+    bib_database_set = [];
 
-        # Process every bibtex file
-        for i in range(1, len(sys.argv)):
+    # Process every bibtex file
+    for i in range(1, len(sys.argv)):
 
-            # Open the file is read it into a string
-            with open(sys.argv[i]) as bibtex_file:
-                bibtex_str = bibtex_file.read()
+        # Open the file is read it into a string
+        with open(sys.argv[i]) as bibtex_file:
+            bibtex_str = bibtex_file.read()
 
-            # Create a bibtex parser
-            parser = BibTexParser()
-            parser.customization = convert_to_unicode
+        # Create a bibtex parser
+        parser = BibTexParser()
+        parser.customization = convert_to_unicode
 
-            # Process the string and add the corresponding BibDatabase into the list
-            bib_database_set.append(bibtexparser.loads(bibtex_str, parser=parser));
+        # Process the string and add the corresponding BibDatabase into the list
+        bib_database_set.append(bibtexparser.loads(bibtex_str, parser=parser));
 
-        # Print the number of BibDatabase objects in bib_database_set
-        print(len(bib_database_set))
+    # Print the number of BibDatabase objects in bib_database_set
+    print(len(bib_database_set))
 
-        # Record the max number of authors
-        max_number_of_authors = 1;
+    # Record the max number of authors
+    max_number_of_authors = 1;
 
-        # Process all the BibDatabase of the list
-        for j in range(len(bib_database_set)):
+    # Process all the BibDatabase of the list
+    for bib_database in bib_database_set:
 
-            # Find the number of authors
-            for entry_key in bib_database_set[j].entries_dict:
-                # Get the authors
-                authors = bib_database_set[j].entries_dict[entry_key]['author'];
+        # Find the number of authors
+        for entry_key in bib_database.entries_dict:
 
-                # Get the number of authors
-                number_of_authors = authors.count(' and ') + 1;
+            # Get the authors
+            authors = bib_database.entries_dict[entry_key]['author'];
 
-                # Update the max number of authors
-                max_number_of_authors = max(number_of_authors, max_number_of_authors);
+            # Get the number of authors
+            number_of_authors = authors.count(' and ') + 1;
+
+            # Update the max number of authors
+            max_number_of_authors = max(number_of_authors, max_number_of_authors);
+
+    # Create databaset structure
+    db = TinyDB('db.json')
+    authors_table = db.table('authors')
+    titles_table = db.table('titles')
+    connections_table = db.table('connections')
+
+    # Process all the BibDatabase of the list
+    for bib_database in bib_database_set:
+        #print("Process ", sys.argv[j + 1])
+        # Print the bibtex entry key, author and title of the corresponding BibDatabase object
+        for entry_key in bib_database.entries_dict:
+
+            # This is a paper in a conference:
+            pub_venue="";
+            if bib_database.entries_dict[entry_key]['ENTRYTYPE'] == "inproceedings":
+                pub_venue = bib_database.entries_dict[entry_key]['booktitle'];
+            elif bib_database.entries_dict[entry_key]['ENTRYTYPE'] == "article":
+                pub_venue = bib_database.entries_dict[entry_key]['journal'];
+            else:
+                pub_venue = "Unknown, ask Franck what ";
+                pub_venue += bib_database.entries_dict[entry_key]['ENTRYTYPE'];
+                pub_venue += " is.";
+
+            # Get the year
+            year = bib_database.entries_dict[entry_key]['year'];
+
+            # Get the booktitle
+            booktitle = bib_database.entries_dict[entry_key]['booktitle'];
+
+            # Get the title
+            title = bib_database.entries_dict[entry_key]['title'];
+
+            # Get the authors
+            authors = bib_database.entries_dict[entry_key]['author'];
+
+            # Get the number of authors
+            number_of_authors = authors.count(' and ') + 1;
+
+            titleid=titleid+1
+            titles_table.insert({'TitleID': titleid,'Title': title,'Booktitle': booktitle,'Year': year})
 
 
-        # Process all the BibDatabase of the list
-        for j in range(len(bib_database_set)):
-            print("Process ", sys.argv[j + 1])
-            # Print the bibtex entry key, author and title of the corresponding BibDatabase object
-            for entry_key in bib_database_set[j].entries_dict:
 
-                # This is a paper in a conference:
-                pub_venue="";
-                if bib_database_set[j].entries_dict[entry_key]['ENTRYTYPE'] == "inproceedings":
-                    pub_venue = bib_database_set[j].entries_dict[entry_key]['booktitle'];
-                elif bib_database_set[j].entries_dict[entry_key]['ENTRYTYPE'] == "article":
-                    pub_venue = bib_database_set[j].entries_dict[entry_key]['journal'];
+            # Find all the authors
+            authors_split = authors.split(' and ');
+            for author in authors_split:
+                name_components = author.split(', ');
+
+                # Look for author ID in authors_table
+                # If author in in, use his/her ID from the table
+
+                # else increment authorid 
+                authorid=authorid+1
+
+                connections_table.insert({'TitleID': titleid,'AuthorID': authorid})
+                #G.add_edge(authorid, titleid)
+                if len(name_components) == 1:
+                    authors_table.insert({'AuthorID': authorid,'Author': author})
                 else:
-                    pub_venue = "Unknown, ask Franck what ";
-                    pub_venue += bib_database_set[j].entries_dict[entry_key]['ENTRYTYPE'];
-                    pub_venue += " is.";
+                    firstnames = name_components[1].split(' ');
+                    short_name = "";
+                    for firstname in firstnames:
+                        short_name += firstname[0];
+                        short_name += ". ";
+                    short_name += name_components[0];
+                    authors_table.insert({'AuthorID': authorid,'Author': short_name})
 
-                # Get the year
-                year = bib_database_set[j].entries_dict[entry_key]['year'];
+            #print("Entry key: ", entry_key)
 
-                # Get the booktitle
-                booktitle = bib_database_set[j].entries_dict[entry_key]['booktitle'];
-
-                # Get the title
-                title = bib_database_set[j].entries_dict[entry_key]['title'];
-
-                # Get the authors
-                authors = bib_database_set[j].entries_dict[entry_key]['author'];
-
-                # Get the number of authors
-                number_of_authors = authors.count(' and ') + 1;
-
-                db = TinyDB('db.json')
-                authors_table = db.table('authors')
-                titles_table = db.table('titles')
-                connections_table = db.table('connections')
-                titleid=titleid+1
-
-                titles_table.insert({'TitleID': titleid,'Title': title,'Booktitle': booktitle,'Year': year})
-                
-                # Find all the authors
-                authors_split = authors.split(' and ');
-                for author in authors_split:
-                    name_components = author.split(', ');
-                    authorid=authorid+1
-                    connections_table.insert({'TitleID': titleid,'AuthorID': authorid})
-                    G.add_edge(authorid, titleid)
-                    if len(name_components) == 1:
-                        authors_table.insert({'AuthorID': authorid,'Author': author})
-                    else:
-                        firstnames = name_components[1].split(' ');
-                        short_name = "";
-                        for firstname in firstnames:
-                            short_name += firstname[0];
-                            short_name += ". ";
-                        short_name += name_components[0];
-                        authors_table.insert({'AuthorID': authorid,'Author': short_name})
-
-                #print("Entry key: ", entry_key)  
-
-                print(db);
-                print("Author(s): ", authors);
-                print("Booktitle: ", booktitle);
-                print("Title: ", title);
-                print("Year: ", year);
-                print();
+            print(db);
+            print("Author(s): ", authors);
+            print("Booktitle: ", booktitle);
+            print("Title: ", title);
+            print("Year: ", year);
+            print();
